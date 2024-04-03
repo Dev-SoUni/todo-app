942ff512048ded59965fee5d80b55025961760a4
Kim JaeSeok <60083524+iai6203@users.noreply.github.com>
2024-04-03 15:38:27 +0900

    feat: MariaDB 데이터베이스 설정 (#7)
    - MariaDB 데이터베이스 설정
    - 데이터베이스의 todo 데이터 전체 조회 함수 추가

---
+++ apps/www/src/lib/db.ts
@@ -0,0 +1,15 @@
+/**
+ * MariaDB 데이터베이스
+ */
+
+import mariadb from "mariadb";
+
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  connectionLimit: 5,
+})
+
+export { pool }
